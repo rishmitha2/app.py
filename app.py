7019bf6cf7bf83317054ce3b9c4160eb641b1e03
Rishmitha <rishmithakb@kssem.edu.in>
2026-04-01 11:30:23 +0530

    Refactor Flask routes to use requests for API calls

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
-from flask import Flask, jsonify
 import requests
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-BASE_URL = "https://jsonplaceholder.typicode.com"
-
-@app.route('/')
-def home():
-    return jsonify({"message": "Using JSONPlaceholder API"})
-
 @app.route('/posts')
-def posts():
-    data = requests.get(f"{BASE_URL}/posts").json()
-    return jsonify(data[:5])
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "successfully triggered auto deployment",
+        "status_code": 200
+    })
 
 @app.route('/comments')
-def comments():
-    data = requests.get(f"{BASE_URL}/comments").json()
-    return jsonify(data[:5])
+def get_comments():
+    url = "https://jsonplaceholder.typicode.com/comments"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
 
 @app.route('/albums')
-def albums():
-    data = requests.get(f"{BASE_URL}/albums").json()
-    return jsonify(data[:5])
+def get_albums():
+    url = "https://jsonplaceholder.typicode.com/albums"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
